7512d59b8f3e8a8f8881d23d2e594bfbb4dc306b
jungseok-corine <nonehour@naver.com>
2023-12-13 17:53:00 +0900

    first commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by 오정석 on 12/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
