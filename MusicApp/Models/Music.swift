52a218c83851fe91547f7b8062c8dac20c5b8a7e
jungseok-corine <nonehour@naver.com>
2023-12-14 23:12:35 +0900

    second commit

---
+++ MusicApp/Models/Music.swift
@@ -0,0 +1,12 @@
+//
+//  Music.swift
+//  MusicApp
+//
+//  Created by 오정석 on 14/12/2023.
+//
+
+import Foundation
+
+struct Music {
+    
+}
---
+++ MusicApp/Views/MusicCell.swift
@@ -0,0 +1,30 @@
+//
+//  MusicCell.swift
+//  MusicApp
+//
+//  Created by 오정석 on 14/12/2023.
+//
+
+import UIKit
+
+final class MusicCell: UITableViewCell {
+
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var songNameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var albumNameLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
