cbf5cf7cd096500d9f05350d9347932a36226bfb
jungseok-corine <nonehour@naver.com>
2023-12-15 10:50:14 +0900

    third commit

--- MusicApp/Controllers/ViewController.swift
+++ MusicApp/Controllers/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    // 🍏 서치 컨트롤러 생성 ===> 네비게이션 아이템에 할당
+//    let searchController = UISearchController()
+    
+    // 🍎 서치 Results 컨트롤러
+    let searchController = UISearchController(searchResultsController: UIStoryboard(name: "Main", bundle: nil))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // 서치바 셋팅
+    func setupSearchBar() {
+        self.title = "Music Search"
+        navigationItem.searchController = searchController
+        
+        // (단순)서치바의 사용
+        searchController.searchBar.delegate = self
+    }
 }
 
+extension ViewController: UISearchBarDelegate {
+    
+}
